bd75d896afbd42f8a2c8f51882af944947376d13
fgolfc <cf.ts.fujimoto@gmail.com>
2023-02-25 21:07:42 +0900

    step1_commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+  
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:notice] = 'Task was successfully created.'
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:notice] = 'Task was successfully updated.'
+      redirect_to @task
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:notice] = 'Task was successfully destroyed.'
+    redirect_to tasks_url
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
