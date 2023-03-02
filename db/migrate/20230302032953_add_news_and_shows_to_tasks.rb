0c8abdfa367d50a188d9ae4bf87047c2bf48468e
fgolfc <cf.ts.fujimoto@gmail.com>
2023-03-02 13:49:47 +0900

    node v-change6

--- db/migrate/20230302032953_add_news_and_shows_to_tasks.rb
+++ db/migrate/20230302032953_add_news_and_shows_to_tasks.rb
@@ -1,6 +1,4 @@
 class AddNewsAndShowsToTasks < ActiveRecord::Migration[6.0]
   def change
-    add_reference :tasks, :news, null: false, foreign_key: true
-    add_reference :tasks, :shows, null: false, foreign_key: true
   end
 end
