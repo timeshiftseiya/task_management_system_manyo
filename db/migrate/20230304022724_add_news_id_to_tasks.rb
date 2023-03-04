b560e58e65a7bca0968f2c3550d4781100880b25
fgolfc <cf.ts.fujimoto@gmail.com>
2023-03-04 16:12:48 +0900

    set null defaltl

--- db/migrate/20230304022724_add_news_id_to_tasks.rb
+++ db/migrate/20230304022724_add_news_id_to_tasks.rb
@@ -1,5 +1,11 @@
 class AddNewsIdToTasks < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    change_column_default :tasks, :news_id, 0
+    change_column_null :tasks, :news_id, false
+  end
+
+  def down
+    change_column_default :tasks, :news_id, nil
     change_column_null :tasks, :news_id, true
   end
 end
