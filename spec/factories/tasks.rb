9c5012c3677c9c210b46f3390b1baa23084aeb1e
fgolfc <cf.ts.fujimoto@gmail.com>
2023-03-05 11:44:17 +0900

    rspec repair

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :task do
-    
+    title { '書類作成' }
+    content { '企画書を作成する。' }
+  end
+  factory :second_task, class: Task do
+    title { 'メール送信' }
+    content { '顧客へ営業のメールを送る。' }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空文字の場合' do
+      it 'バリデーションに失敗する' do
+        task = Task.create(title: '', content: '企画書を作成する。')
+        expect(task).not_to be_valid
+      end
+    end
+
+    context 'タスクの説明が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        task = Task.create(title: '書類作成', content: '')
+        expect(task).not_to be_valid
+      end
+    end
+
+    context 'タスクのタイトルと説明に値が入っている場合' do
+      it 'タスクを登録できる' do
+        task = Task.create(title: '書類作成', content: '企画書を作成する。')
+        expect(task).to be_valid
+      end
+    end
+  end
 end
